3fcf2bdad307ae29af0feb765595dd8246d8b39b
Louise Hamerston <louiserosehamerston@gmail.com>
2019-07-09 20:21:01 +0100

    Changed settings file for deployment

--- wedsite/settings.py
+++ wedsite/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9^sdh%3$3$r02(3i&h^kh+sinh)j(-l%r&&w-(a_g$w=)*(*1d'
+# SECRET_KEY = '9^sdh%3$3$r02(3i&h^kh+sinh)j(-l%r&&w-(a_g$w=)*(*1d'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+export DJANGO_DEBUG=False
 
 ALLOWED_HOSTS = []
 
