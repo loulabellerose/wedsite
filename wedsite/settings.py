348c081e47b9ff450e7403d673718c2562beb520
Louise Hamerston <louiserosehamerston@gmail.com>
2019-07-10 20:11:39 +0100

    Added domain name to allowed_hosts

--- wedsite/settings.py
+++ wedsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1','fierce-atoll-85493.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','fierce-atoll-85493.herokuapp.com', 'www.impossiblynow.com']
 
 
 # Application definition
