7bf01024006b14f08465f503d6ae27b91135dd44
Louise Hamerston <louiserosehamerston@gmail.com>
2019-07-11 22:28:48 +0100

    Rolling back changes to debug settings

--- wedsite/settings.py
+++ wedsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','fierce-atoll-85493.herokuapp.com', 'www.impossiblynow.com', "impossiblynow.com"]
 
