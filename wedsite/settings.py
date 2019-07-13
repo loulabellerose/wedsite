841c7333882ec90bf864ea76c7f01cb3502247ac
Louise Hamerston <louiserosehamerston@gmail.com>
2019-07-13 23:39:20 +0100

    Ran collect static

--- wedsite/settings.py
+++ wedsite/settings.py
@@ -134,7 +134,7 @@ DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
